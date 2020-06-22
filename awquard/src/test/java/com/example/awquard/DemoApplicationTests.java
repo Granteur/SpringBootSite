a985177de9191152c585f1f93c718536264f4a2f
AJ Grant <angelojgrant@gmail.com>
2020-06-22 00:23:23 -0400

    first commit

---
+++ awquard/src/test/java/com/example/awquard/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.awquard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
